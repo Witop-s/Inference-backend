b23579036ef34a8b9570d02da716e649096a9968
Witop-s <noahesteve.nicolas@gmail.com>
2025-06-24 22:00:24 +0200

    test

--- function_app.py
+++ function_app.py
@@ -3,13 +3,6 @@ logging.warning("⚠️ function_app.py loaded")
 
 import azure.functions as func
 
-# Importer les handlers
-from functions.main_inner_voice import inner_voice
-from functions.main_fact_filter import inner_voice_fact_filter
-from functions.main_inspector import inspector
-from functions.main_get_scenario import get_scenario
-from functions.main_endgame import endgame
-
 app = func.FunctionApp()
 
 @app.function_name(name="ping")
