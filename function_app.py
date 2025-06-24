aad3ea26ba147c65d70fbcfc66074e994b613a03
Witop-s <noahesteve.nicolas@gmail.com>
2025-06-24 22:50:44 +0200

    pfff

--- function_app.py
+++ function_app.py
@@ -2,6 +2,7 @@ import logging
 logging.warning("⚠️ function_app.py loaded")
 
 import azure.functions as func
+app = func.FunctionApp()
 
 # Importer les handlers
 try:
@@ -28,14 +29,6 @@ try:
     from handlers.main_endgame import endgame
 except Exception as e:
     logging.error(f"❌ endgame failed to import: {e}")
-
-app = func.FunctionApp()
-
-@app.function_name(name="ping")
-@app.route(route="ping", auth_level=func.AuthLevel.ANONYMOUS)
-def ping(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("ping called")
-    return func.HttpResponse("pong", status_code=200)
 
 @app.function_name(name="ping")
 @app.route(route="ping", auth_level=func.AuthLevel.ANONYMOUS)
