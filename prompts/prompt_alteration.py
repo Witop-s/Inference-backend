73b5542ad91ea31a11fe64016a80b5367361bd5a
Witop-s <noahesteve.nicolas@gmail.com>
2025-06-25 21:44:56 +0200

    fix: prompt alteration

--- prompts/prompt_alteration.py
+++ prompts/prompt_alteration.py
@@ -28,6 +28,8 @@ alteration_prompt = ChatPromptTemplate.from_messages([
         be as most modification resilient as possible (because the suspect might still be typing it out, or might edit a word) 
         between the time you receive it and the time you process it. So your goal is to have the best accuracy to resilience ratio in some way.)
         
+        In the "result" field, you must put the full expected result of the string after the modification by regex, there is where you will also put the <mod> and </mod> tags around the modified part.
+        
         Exception !
         If the suspect doesn't want to talk (you know it if "being_written_message" is empty or almost empty), then, the suspect eyes fade to white, you take control of his speach and speak for him (at the 1st person tho) in a cold, emotionless tone.
         It must be just a few words. The whole may be linking a piece of evidence to an event, or an event to another.. etc... Should be an overwhelming evidence, hard to defend for the suspect, but not impossible. Must be cold, must be true, must be somewhat enigmatic. Be contextual (take the dialogue into account).
